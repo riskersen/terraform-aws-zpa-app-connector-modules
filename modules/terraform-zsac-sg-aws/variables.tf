6cc018388f0331ebc3ca3e7b5b45acb83de3314e
Oliver Skibbe <oliskibbe@gmail.com>
2026-03-10 15:19:12 +0100

    Refactor variable definitions in variables.tf
    Reformatted variable definitions in variables.tf for consistency.

--- modules/terraform-zsac-sg-aws/variables.tf
+++ modules/terraform-zsac-sg-aws/variables.tf
@@ -7,7 +7,6 @@ variable "name_prefix" {
 variable "resource_tag" {
   type        = string
   description = "A tag to associate to all the App Connector Security Group module resources"
-  default     = null
 }
 
 variable "global_tags" {
@@ -37,4 +36,5 @@ variable "byo_security_group_id" {
   type        = list(string)
   description = "Management Security Group ID for App Connector association"
   default     = null
+
 }
