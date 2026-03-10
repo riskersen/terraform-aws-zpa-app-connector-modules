128c0b4004c62eed059eaaa9994fef9a12524557
Oliver Skibbe <oliskibbe@gmail.com>
2026-03-10 15:19:00 +0100

    Remove default value for resource_tag variable
    Removed default value for resource_tag variable.

--- modules/terraform-zsac-network-aws/variables.tf
+++ modules/terraform-zsac-network-aws/variables.tf
@@ -7,7 +7,6 @@ variable "name_prefix" {
 variable "resource_tag" {
   type        = string
   description = "A tag to associate to all the network module resources"
-  default     = null
 }
 
 variable "global_tags" {
